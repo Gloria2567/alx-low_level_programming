94408990c28ca4048265694072bff2a50b2759c6
Gloria2567 <glorianamono6@gmail.com>
2022-12-23 22:22:46 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,16 +1,31 @@
 #include "main.h"
 
+
+
 /**
- * main - check the code
  *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
 int main(void)
+
 {
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
+
+	    print_t0_98(0);
+
+	        print_t0_98
+
+		    print_t0_98(111);
+
+		        print_t0_98(81);
+
+			    print_t0_98(-10);
+
+			        return (0);
+
 }
